d5bd32914ba649568b0b92e845ec785cbd87b5fa
Akhilesh Patel <akhilpatel2121@gmail.com>
2025-03-08 22:16:00 +0530

    update docker file

--- simple-registration2.0/cache-clear.sh
+++ simple-registration2.0/cache-clear.sh
@@ -5,6 +5,6 @@ php artisan config:clear && \
 php artisan route:clear && \
 php artisan view:clear && \
 php artisan event:clear && \
-php artisan clear-compiled && \
-php artisan optimize:clear
+php artisan clear-compiled 
+#php artisan optimize:clear
 
